b600c16b45ee8121b9b79ce194c324335778c831
Cem Geçgel <gecgelcem@outlook.com>
2022-01-12 08:56:55 +0300

    Implemented Keyboard

---
+++ Keyboard.c
@@ -0,0 +1,85 @@
+#include "Keyboard.h"
+
+#include "MemoryMap.h"
+
+#define KEYBOARD_KEY_TANK_LEFT 0x1C    // A
+#define KEYBOARD_KEY_TANK_RIGHT 0x23   // D
+#define KEYBOARD_KEY_BARREL_LEFT 0x24  // E
+#define KEYBOARD_KEY_BARREL_RIGHT 0x15 // Q
+#define KEYBOARD_KEY_POWER_UP 0x1D     // W
+#define KEYBOARD_KEY_POWER_DOWN 0x1B   // S
+#define KEYBOARD_KEY_SHOOT 0x29	       // SPACE
+#define KEYBOARD_BREAK_CODE 0xF0
+
+/** Holds the key data. */
+static struct {
+	bool tankLeft;
+	bool tankRight;
+	bool barrelLeft;
+	bool barrelRight;
+	bool powerUp;
+	bool powerDown;
+	bool shoot;
+} keyboard;
+/** The previous byte in the buffer. Says if it is a press or a release. */
+static volatile uint8_t previous_data;
+
+void keyboard_config(void) {
+	/* Start with a zero value. */
+	previous_data = 0;
+}
+void keyboard_update(void) {
+	Register32 PS2 = (Register32)0xFF200100;
+	uint32_t data = *PS2;
+	uint32_t const RVALID = data & 0x8000;
+	if (RVALID) {
+		bool keyState = previous_data != KEYBOARD_BREAK_CODE;
+		previous_data = data & 0xFF;
+		switch (previous_data) {
+		case KEYBOARD_KEY_TANK_LEFT:
+			keyboard.tankLeft = keyState;
+			break;
+		case KEYBOARD_KEY_TANK_RIGHT:
+			keyboard.tankRight = keyState;
+			break;
+		case KEYBOARD_KEY_BARREL_LEFT:
+			keyboard.barrelLeft = keyState;
+			break;
+		case KEYBOARD_KEY_BARREL_RIGHT:
+			keyboard.barrelRight = keyState;
+			break;
+		case KEYBOARD_KEY_POWER_UP:
+			keyboard.powerUp = keyState;
+			break;
+		case KEYBOARD_KEY_POWER_DOWN:
+			keyboard.powerDown = keyState;
+			break;
+		case KEYBOARD_KEY_SHOOT:
+			keyboard.shoot = keyState;
+			break;
+		default:
+			break;
+		}
+	}
+}
+bool keyboard_tank_left(void) {
+	return keyboard.tankLeft;
+}
+bool keyboard_tank_right(void) {
+	return keyboard.tankRight;
+}
+bool keyboard_barrel_left(void) {
+	return keyboard.barrelLeft;
+}
+bool keyboard_barrel_right(void) {
+	return keyboard.barrelRight;
+}
+bool keyboard_power_up(void) {
+	return keyboard.powerUp;
+}
+bool keyboard_power_down(void) {
+	return keyboard.powerDown;
+}
+bool keyboard_shoot(void) {
+	return keyboard.shoot;
+}
--- Keyboard.h
+++ Keyboard.h
@@ -2,13 +2,25 @@
 #define KEYBOARD_H
 
 #include <stdbool.h>
-#include <stdint.h>
 
 /** Configures and initializes the keyboard system. Called at the start of the
  * program. */
 void keyboard_config(void);
-/** Returns wether the key that corresponds to the given id is currently
- * pressed down. Called by the game logic. */
-bool keyboard_down(uint8_t key);
+/** Updates the keyboard, called every frame. */
+void keyboard_update(void);
+/** Returns true if the key is pressured to move the tank left. */
+bool keyboard_tank_left(void);
+/** Returns true if the key is pressured to move the tank right. */
+bool keyboard_tank_right(void);
+/** Returns true if the key is pressured to turn the barrel left. */
+bool keyboard_barrel_left(void);
+/** Returns true if the key is pressured to turn the barrel right. */
+bool keyboard_barrel_right(void);
+/** Returns true if the key is pressured to turn the power up. */
+bool keyboard_power_up(void);
+/** Returns true if the key is pressured to turn the power down. */
+bool keyboard_power_down(void);
+/** Returns true if the key is pressured to shoot the bullet. */
+bool keyboard_shoot(void);
 
 #endif // KEYBOARD_H
