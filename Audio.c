706e8dc5811aac81cab600b631791fa95104ac90
Ahmet İhsan Gül <ihsangul98@gmail.com>
2022-01-12 20:58:41 +0300

    Turned AudioTest into static, separated its crucial contents into other two files

---
+++ Audio.c
@@ -0,0 +1,35 @@
+#include "Audio.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+
+void audio_config(void){
+    volatile int *audio_ptr = (int *) 0xFF203040;
+    *audio_ptr |= 0b1000;
+    *audio_ptr &= ~0b1111;
+}
+
+void audio_play(int input_buf_size, int* input_array){
+    volatile int *audio_ptr = (int *) 0xFF203040;
+    AUDIO_BUF_SIZE = input_buf_size;
+    audio_data = input_array;
+    *audio_ptr = *audio_ptr | 0b10;
+}
+
+void audio_ISR (void){                        
+    volatile int *audio_ptr = (int *) 0xFF203040;
+
+    int fifospace = *(audio_ptr + 1);
+	int wsrc = (fifospace & 0x00FF0000) >> 16;
+		while ((wsrc >= 32) && (buffer_index < AUDIO_BUF_SIZE)){
+			*(audio_ptr+2) = audio_data[buffer_index];
+			*(audio_ptr+3) = audio_data[buffer_index];
+			buffer_index += 3;
+			fifospace = *(audio_ptr + 1);
+			wsrc = (fifospace & 0x00FF0000) >> 16;
+		}
+    if(buffer_index >= AUDIO_BUF_SIZE){
+        *audio_ptr = *audio_ptr & ~0b10;
+		buffer_index = 0;
+	}
+}
