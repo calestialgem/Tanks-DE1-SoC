0509205aacc8b885dfed438093073eea908e329f
Ahmet İhsan Gül <ihsangul98@gmail.com>
2022-01-13 11:26:24 +0300

    Refined the definitions for each function

--- Audio.h
+++ Audio.h
@@ -26,13 +26,16 @@ volatile int buffer_index = 0;
 /* Clears the audio write register */
 void audio_config(void);
 
-/* Plays sound until the whole sound is played, depending on the pressed key. 
+/* Defines which sound to be played by assigning the array. 
+Enables the interrupt bit in the audio register.
 Sound's array and array's length should be given as inputs */
 void audio_play(int input_buf_size, int* input_array);
 
-/* Stops playing the sound. Should be put into the "Is key released?" check. */
+/* Stops playing the sound by disabling the interrupt bit in the audio register.
+Should be put into the "Is key released?" check. */
 void audio_stop(void);
 
+/* Takes which sound to be played from "audio_play" and plays it until the whole sound is completed.*/
 void audio_ISR (void);
 
 #endif // AUDIO_H
