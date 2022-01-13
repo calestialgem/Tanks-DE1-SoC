7f224c196be86246af658566b050c7123c1e9859
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-13 18:44:07 +0300

    Unique Colors

--- Main.c
+++ Main.c
@@ -5,6 +5,8 @@
 #include "Keyboard.h"
 #include "Timer.h"
 
+#define TANK_COUNT 3
+
 /** Configures all the subsystems at the start. */
 static void config_all(void) {
 	error_config();
@@ -12,13 +14,31 @@ static void config_all(void) {
 	timer_config();
 	graphics_initialize();
 }
-
+static void add_tanks(void) {
+	size_t colors[TANK_COUNT];
+	size_t i;
+	for (i = 0; i < TANK_COUNT; i++) {
+		bool new;
+		do {
+			colors[i] =
+				math_random_index(GRAPHICS_TANK_COLOR_COUNT);
+			new = false;
+			size_t j;
+			for (j = 0; j < i; j++) {
+				if ((new = colors[i] == colors[j])) {
+					break;
+				}
+			}
+		} while (new);
+	}
+	for (i = 0; i < TANK_COUNT; i++) {
+		tank_add(colors[i]);
+	}
+}
 /** Starts the program. */
 int main() {
 	math_reseed();
-	tank_add(math_random_index(GRAPHICS_TANK_COLOR_COUNT));
-	tank_add(math_random_index(GRAPHICS_TANK_COLOR_COUNT));
-	tank_add(math_random_index(GRAPHICS_TANK_COLOR_COUNT));
+	add_tanks();
 	game_restart();
 	config_all();
 	while (true) {
